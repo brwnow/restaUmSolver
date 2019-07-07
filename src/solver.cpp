dc5186a5e0609d9f487004ea471e41f40a0e38da
Bruno Garcia <brwnow@gmail.com>
2019-07-07 17:35:45 -0300

    Implementing Solver member methods

--- src/solver.cpp
+++ src/solver.cpp
@@ -6,3 +6,38 @@ Solver::Solver(GameState initialState) :
 {
 
 }
+
+weak_ptr<const list<GameState>> Solver::getSolution() const {
+    return weak_ptr<const list<GameState>>(solution);
+}
+
+bool Solver::solve() {
+    solution = shared_ptr<list<GameState>>(new list<GameState>());
+
+    return recursiveSolve(initialState);
+}
+
+bool Solver::recursiveSolve(GameState currentState) {
+    if(currentState.getPiecesCount() == 1) {
+        solution->push_front(currentState);
+
+        return true;
+    } else {
+        list<GameState> *childStates = currentState.getChildStatesList();
+        bool success = false;
+
+        for(list<GameState>::iterator it = childStates->begin(); it != childStates->end(); ++it) {
+            success = recursiveSolve(*it);
+
+            if(success)
+                break;
+        }
+
+        if(success)
+            solution->push_front(currentState);
+
+        delete childStates;
+
+        return success;
+    }
+}
